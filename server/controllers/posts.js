1e5eecf1a90e804882d97c37ca609624676da730
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2021-05-30 14:58:14 +0600

    Patch method implement for update

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,5 @@
 import PostMessage from '../models/postMessage.js'
+import mongoose from 'mongoose'
 
 export const getPosts = async (req, res) => {
   try {
@@ -21,4 +22,16 @@ export const createPost = async (req, res) => {
   } catch (error) {
     res.status(409).json({ message: error.message })
   }
+}
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params
+  const post = req.body
+
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send('no post with that id')
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true })
+
+  res.json(updatedPost)
 }
