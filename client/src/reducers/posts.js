07c706a945494bf1fb851a4532b60255e47bbd94
mazba.turza <turza1994@yahoo.com>
2021-05-30 19:59:02 +0600

    Fix

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,7 +1,6 @@
 export default (posts = [], action) => {
   switch (action.type) {
-    case 'FETCH_ALL':
-      return action.payload
+    
 
     case 'CREATE':
       return [...posts, action.payload]
@@ -11,9 +10,7 @@ export default (posts = [], action) => {
         post._id === action.payload._id ? action.payload : post
       )
 
-    case 'DELETE':
-      return posts.filter((post) => post._id !== action.payload)
-
+    
     case 'LIKE':
       return posts.map((post) =>
         post._id === action.payload._id ? action.payload : post
