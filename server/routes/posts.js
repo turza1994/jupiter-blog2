d25d41ea9335a81f9e1e44f9dee3a4511e6d5c73
mazba.turza <turza1994@yahoo.com>
2021-05-30 19:54:25 +0600

    Modify controllern and router

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -9,10 +9,9 @@ import {
 
 const router = express.Router()
 
-router.get('/', getPosts)
+
 router.post('/', createPost)
 router.patch('/:id', updatePost)
-router.delete('/:id', deletePost)
 router.patch('/:id/likePost', likePost)
 
 export default router
