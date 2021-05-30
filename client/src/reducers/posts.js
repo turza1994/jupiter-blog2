05e4fd0f0c90b84a7804a483ce931e4fe8d866ea
mazba.turza <turza1994@yahoo.com>
2021-05-30 19:59:58 +0600

    add post reducer

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,6 +1,7 @@
 export default (posts = [], action) => {
   switch (action.type) {
-    
+    case 'FETCH_ALL':
+      return action.payload
 
     case 'CREATE':
       return [...posts, action.payload]
@@ -10,7 +11,9 @@ export default (posts = [], action) => {
         post._id === action.payload._id ? action.payload : post
       )
 
-    
+    case 'DELETE':
+      return posts.filter((post) => post._id !== action.payload)
+
     case 'LIKE':
       return posts.map((post) =>
         post._id === action.payload._id ? action.payload : post
