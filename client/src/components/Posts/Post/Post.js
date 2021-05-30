f785064aefb5a53a45dc9a2466d296ef872e8f6e
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2021-05-30 15:23:15 +0600

    Delete feature from front end

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -12,10 +12,15 @@ import DeleteIcon from '@material-ui/icons/Delete'
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
 import moment from 'moment'
 import { useDispatch } from 'react-redux'
+import {deletePost} from '../../../actions/posts'
 import useStyles from './styles'
 
 const Post = ({ post, setCurrentId }) => {
   const classes = useStyles()
+  const dispatch = useDispatch()
+
+
+
   return (
     <Card className={classes.card}>
       <CardMedia
@@ -77,7 +82,7 @@ const Post = ({ post, setCurrentId }) => {
         <Button size='small' color='primary' onClick={() => {}}>
           <ThumbUpAltIcon fontSize='small' /> Like {post.likeCount}{' '}
         </Button>
-        <Button size='small' color='primary' onClick={() => {}}>
+        <Button size='small' color='primary' onClick={() => dispatch(deletePost(post._id))}>
           <DeleteIcon fontSize='small' /> Delete
         </Button>
       </CardActions>
